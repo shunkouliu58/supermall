34e41e74275e57fbe5f8619ea5f9b6c01bc9bb9f
shunkouliu <2974967055@qq.com>
2020-08-06 16:59:10 +0800

    首页使用Better-scroll

---
+++ src/components/content/goods/GoodsList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="goods">
+    <goods-list-item v-for="item in goods" : goods-item="item"></goods-list-item>
+  </div>
+</template>
+
+<script>
+  import GoodsIListtem from "./GoodsIListtem";
+
+  export default {
+    name: "GoodsList",
+    components: {
+      GoodsIListtem
+    },
+    props: {
+      goods: {
+        type: Array,
+        default() {
+          return []
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .goods {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    padding: 2px;
+  }
+</style>
--- src/views/home/childComps/RecommendView.vue
+++ src/views/home/childComps/RecommendView.vue
@@ -28,13 +28,19 @@
   .recommend {
     display: flex;
     width: 100%;
+    text-align: center;
+    font-size: 12px;
+
+    padding: 10px 0 20px;
+    border-bottom: 10px solid #eee ;
   }
   .recommend-item {
     flex: 1;
   }
 
   .recommend-item img{
-    width: 65px;
-    height: 65px;
+    width: 68px;
+    height: 68px;
+    margin-bottom: 10px;
   }
 </style>
